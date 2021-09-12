d48510531687892bab86d85359a35d6540efac4e
Grace <jequel07@gmail.com>
2021-09-12 19:32:06 -0400

    adding answers

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,43 @@
 #write your code here
+def echo(phrase)
+    "#{phrase}"
+end
+
+def shout(phrase)
+    "#{phrase.upcase}"
+end
+
+def repeat(phrase, number=2)
+    complete_phrase = phrase + " "
+    complete_phrase = complete_phrase * number
+    complete_phrase.strip
+end
+puts repeat("hello", 3)
+  
+  
+def start_of_word(string, number)
+    string[0..number-1]
+end
+  
+puts start_of_word("abcdefg",2)
+  
+def first_word(string)
+    string.split.first
+end
+  
+puts first_word("Hello world")
+  
+def titleize(string)
+    little_words = ["the", "and", "over"]
+      string = string.capitalize 
+    word_array = string.split(" ")
+    capitialized_words_in_array = word_array.each do|word| 
+      if little_words.include?(word)
+      else
+        word.capitalize! # if word is not the and over then apitalize
+      end
+    end
+    capitialized_words_in_array.join(" ")
+end
+  
+puts titleize("the bridge over the river kwai")
--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,22 @@
+def time_string_helper(seconds)
+	seconds_format=seconds % 60 #40
+	minutes_format= (seconds / 60) > 60 ? (seconds / 60) - 60 : (seconds / 60) #06
+	hours_format=seconds / 3600 #1
+	
+	seconds_formatted = seconds_format > 9 ? seconds_format : "0#{seconds_format}"
+	minutes_formatted = minutes_format > 9 ? minutes_format : "0#{minutes_format}"
+	hours_formatted = hours_format > 9 ? hours_format : "0#{hours_format}"
+	"#{hours_formatted}:#{minutes_formatted}:#{seconds_formatted}"
+end
+
 class Timer
-  #write your code here
+	attr_accessor :seconds
+
+	def initialize()  #with def initialize can remove te @ symbol though attr reader
+		@seconds = 0	
+	end
+
+	def time_string
+		time_string_helper(seconds)
+	end
 end
